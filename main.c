c344f90539422824d1073b44f67d0903936d6931
Danny Panasyuk <amazer97@gmail.com>
2012-04-29 14:42:19 -0700

    0.2 release, major overhaul. Fixed double-printing bug, removed writing to files, removed real-time editing, implemented better file manipulation/loading, finished scroll / cursor movement.

--- main.c
+++ main.c
@@ -18,24 +18,38 @@ void parse_file(char* file_name)
 {
   FILE *f_ptr;
   int c;
-  unsigned int i;
+  char write_mode = 0;
+  unsigned int i = 0;
   int x;
   unsigned int row = 0;
   char x_cursor = 11;
   char y_cursor = 1;
   char at_cursor;
-  char hex_buff[17];
-  f_ptr = fopen(file_name, "r+");
+  unsigned int sz;
+  char* hex_buff;
+  f_ptr = fopen(file_name, "r+b");
   if(f_ptr == NULL)
   {
     printw("\n The file you were trying to open cannot be opened.");
     return;
   }
+  fseek(f_ptr, 0, SEEK_END);
+  sz = ftell(f_ptr);
+  rewind(f_ptr);
+  hex_buff = (char*) malloc(sz * sizeof(char));
+  do
+  {
+    c = fgetc(f_ptr);
+    hex_buff[i] = c;
+    i++;
+  }while(c != EOF);
+  fclose(f_ptr);
   for(;;)
   {
     clear();
-    mvprintw(0, 0, "Goddamn Hex Tool 0.1 %i %i\n", y_cursor, x_cursor);
-    dump_hex(f_ptr, row, 24, 1); 
+    dump_hex(hex_buff, row, 24, 1);
+    at_cursor = mvinch(y_cursor, x_cursor);
+    mvprintw(0, 0, "Goddamn Hex Tool 0.2 Write Enable:%i\n", write_mode);
     move(y_cursor, x_cursor);
     c = getch();
     switch(c) //Scroll up / down
@@ -45,11 +59,11 @@ void parse_file(char* file_name)
 	  row--;
 	break;
       case KEY_NPAGE:
-	if(row != 0xFFFFFFFF)
+	if(row != sz / 16)
 	  row++;
 	break;
       case 27: //27 == ESCAPE key
-	fclose(f_ptr);
+	free(hex_buff);
 	return;
       case KEY_UP:
 	if(y_cursor != 1)
@@ -64,43 +78,104 @@ void parse_file(char* file_name)
 	  x_cursor--;
 	break;
       case KEY_RIGHT:
-	if(x_cursor != 49)
+	if(x_cursor != 57)
 	  x_cursor++;
+	break;
+      case KEY_F(2):
+	if(write_mode == 1)
+	  write_mode = 0;
+	else
+	  write_mode = 1;
+	break;
+      default:
+	if(write_mode != 0) //Unstable, don't write to files
+	{
+	  if(c >= 48 && c <= 57)//Is c a number character
+	    write_hex(hex_buff, row + y_cursor, y_cursor, x_cursor, c);
+	  if(c >= 97 && c <= 102)//Is a hex character
+	    write_hex(hex_buff, row + y_cursor, y_cursor, x_cursor, c);
+	}
 	break;
     }
   }
 }
 
-void dump_hex(FILE* f_ptr,unsigned int row, unsigned int line_dump, int y)
+void dump_hex(char* buff,unsigned int row, unsigned int line_dump, int y)
 {
   char hex_buff[17];
   int i;
   int x;
-  if(f_ptr == NULL)
-  {
-    printw("\n The file you were trying to open cannot be opened.");
-    return;
-  }
+  int z;
+  int eof_check = 0;
+  
   for(i = y; i < line_dump; i++)
   {
-    fseek(f_ptr, (row + i) * 16, SEEK_SET);
+    z = (row + i) * 16;
     for(x = 0; x < 16; x++)
     {
-      hex_buff[x] = fgetc(f_ptr);
+      hex_buff[x] = buff[z++];
       if(hex_buff[x] == EOF)
-	hex_buff[x] = ' ';
+	continue;
       if(hex_buff[x] < 32)
 	hex_buff[x] = '.';
     }
     hex_buff[16] = '\0';
     mvprintw(i, 0, "%X: ", (row + i) * 16);
     move(i, 11);
-    for(x = 0; x < 8; x++)
+    for(x = 0; x < 16; x++)
     {
-      printw("%X%X ", hex_buff[x], hex_buff[x + 1]);
+      if(hex_buff[x] == EOF)
+      {
+	hex_buff[x + 1] = '\0';
+	mvprintw(i, 60, "%s\n", hex_buff);
+	return;
+      }
+      printw("%X ", hex_buff[x]);
     }
     printw(" %s\n", hex_buff);
   }
   refresh();
 }
 
+void write_hex(char* buff, unsigned int row, char y, char x, char byte) //Commented out due to being unstable
+{
+  /*
+  char hex_buff[2];
+  char c;
+  int i;
+  unsigned int z;
+  int loc;
+  int len;
+  do
+  {
+    c = mvinch(y, x);
+    x++;
+    if(c != ' ')
+      len++;
+  }while(len != (x - 11));
+    if(mvinch(y, x+1) == ' ')
+    {
+     hex_buff[1] = byte;
+     hex_buff[0] = mvinch(y, x-1);
+     loc = -1;
+    }
+    else
+    {
+     hex_buff[0] = byte;
+     hex_buff[1] = mvinch(y, x+1);
+     loc = 0;
+    }
+  for(i = 0; i < 2; i++)
+  {
+    if(hex_buff[i] >= 48 && hex_buff[i] <= 57)
+      hex_buff[i] = hex_buff[i] - 48;
+    else
+      hex_buff[i] = hex_buff[i] - 97;
+  }
+  byte = (hex_buff[0] << 4) | hex_buff[1];
+  z = ((row + y) * 16) + len + loc;
+  buff[z] = byte;
+  //fseek(f_ptr, ((row + y) * 16) + (len + loc), SEEK_SET);
+  //fputc(byte, f_ptr);
+  */
+}
--- main.h
+++ main.h
@@ -2,9 +2,11 @@
 #define _MAIN_H_
 
 #include "stdio.h"
+#include "stdlib.h"
 #include "curses.h"
 
 void parse_file(char* file_name);
-void dump_hex(FILE* f_ptr, unsigned int row, unsigned int line_dump, int y);
+void dump_hex(char* buff, unsigned int row, unsigned int line_dump, int y);
+void write_hex(char* buff, unsigned int row, char y, char x, char byte);
 
 #endif
